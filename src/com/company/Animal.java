33dbcc529efae1cf3fe4d4543e9246449dfb8015
Łukasz <64524258+LukaszSWSB@users.noreply.github.com>
2020-05-02 20:10:37 +0200

    Zadanie 1.3 skonczone

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,29 @@
 package com.company;
 
 public class Animal {
+    /*final String species;*/
+    private double weight = 20;
+
+    public Animal () {
+        double weight = 20;
+        this.weight = weight;
+    }
+    void feed()
+    {
+        double weight1 = 10;
+        double x = this.weight + weight1;
+        System.out.println("Waga została zwieszkona o: " + x);
+    }
+    void takeForAWalk()
+    {
+        double weight2 = 10;
+        if (weight2 < this.weight) {
+            double y = this.weight + weight2;
+            System.out.println("Waga została zmniejszona o: " + y);
+        }
+        else
+        {
+            System.out.println("Zwierze nieżyje");
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a = 0;
+        Scanner scan = new Scanner(System.in);
+        Animal animal = new Animal();
+        do {
+            System.out.println("Choose number");
+            System.out.println("1. Feeder");
+            System.out.println("2. TakeForAWalk");
+            System.out.print("Your Choose: ");
+            a = scan.nextInt();
+            switch (a) {
+                case 1: {
+                    animal.feed();
+                }
+                break;
+                case 2: {
+                    animal.takeForAWalk();
+                }
+                break;
+                default: {
+                    System.out.println("default");
+                }
+                break;
+            }
+        }while (a != 0) ;
     }
 }

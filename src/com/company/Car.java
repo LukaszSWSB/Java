fb108ea68bfea76fcb22ad4a85bc79f55ac3622a
Łukasz <64524258+LukaszSWSB@users.noreply.github.com>
2020-05-03 14:29:45 +0200

    Zadanie 2 skonczone

---
+++ src/com/company/Car.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Car {
+    final String model = "718 Cayman GT4";
+    final String producent = "Porsche";
+    final String color = "Gold";
+    private int production_date = 2019;
+}
--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -2,4 +2,5 @@ package com.company;
 
 public class Human {
     Animal pet = new Animal();
+    Car car = new Car();
 }

49c6c1d20073dcf707db8f195eb20cf9f88d760a
Łukasz <64524258+LukaszSWSB@users.noreply.github.com>
2020-05-03 18:36:27 +0200

    Zadanie 3 i 4 skonczone

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,6 +1,40 @@
 package com.company;
 
 public class Human {
+    public Human() {
+        salary = 1000;
+    }
+    private double salary;
+    double date = 11.05;
     Animal pet = new Animal();
     Car car = new Car();
+
+    double getSalary()
+    {
+        System.out.println("Payment of the day: " + date);
+        System.out.println("Value before returning value= " + salary);
+        Deposit(100);
+        System.out.println("Current Account status= " + salary);
+        return salary;
+    }
+    boolean setSalary(double HowMuch)
+    {
+        if (salary < HowMuch) {
+            System.out.println("You dont have cash");
+            System.out.println("Current Account status= " + salary);
+        }
+        else {
+            System.out.println("New data has been sent to the accounting system");
+            System.out.println("Receive the annex to the contract from Ms. Hania from the staff");
+            System.out.println("ZUS and US already know about the change in payout and it makes no sense to hide your income");
+            this.salary = salary - HowMuch;
+            System.out.println("Current Account status= " + salary);
+        }
+        return true;
+    }
+    boolean Deposit(double HowMuch)
+    {
+        this.salary = salary + HowMuch;
+        return true;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,14 @@ public class Main {
         int a;
         Scanner scan = new Scanner(System.in);
         Animal animal = new Animal();
+        Human human = new Human();
         do {
             System.out.println("Choose number");
             System.out.println("1. Feeder");
             System.out.println("2. TakeForAWalk");
+            System.out.println("3. Check account balance");
+            System.out.println("4. Withdraw from account 500");
+            System.out.println("5. Withdraw from account 1500");
             System.out.print("Your Choose: ");
             a = scan.nextInt();
             switch (a) {
@@ -23,6 +27,18 @@ public class Main {
                     animal.takeForAWalk();
                     if (animal.isLife() == false)
                     { a = 0; }
+                }
+                break;
+                case 3: {
+                    human.getSalary();
+                }
+                break;
+                case 4: {
+                    human.setSalary(500);
+                }
+                break;
+                case 5: {
+                    human.setSalary(1500);
                 }
                 break;
                 default: {

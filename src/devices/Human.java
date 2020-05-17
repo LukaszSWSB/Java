3a727e184d17ff565f83a6e4dfdb943b8f454517
Łukasz <64524258+LukaszSWSB@users.noreply.github.com>
2020-05-17 18:10:15 +0200

    ulepszenie o Tworzenie osób

--- src/devices/Human.java
+++ src/devices/Human.java
@@ -1,16 +1,136 @@
 package devices;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import creatures.Animal;
 
 public class Human {
-    public Human() {
-        salary = 1000;
+    public Human(int id, String name, double salary) {
+        this.id = id;
+        this.Name = name;
+        this.salary = salary;
     }
-    private double salary;
+
+    public Human() {
+    }
+
+    int id = 0, tmp, a;
+    private double salary = 1000;
+    public String Name, Name2;
+    public boolean created = false;
     double date = 11.05;
     Animal pet;
+    Animal animal = new Animal();
     private Car car = new Car();
+    public static ArrayList<Human> human = new ArrayList();
     double Cash;
+    Scanner scan = new Scanner(System.in);
+
+    public ArrayList<Human> NewPerson() {
+        System.out.println("ID: " + id);
+        System.out.print("Name: ");
+        Name = scan.nextLine();
+        created = true;
+        if (created) {
+            human.add(new Human(id, Name, salary));
+            id++;
+            return human;
+        }
+        return human;
+    }
+
+    public void Person()
+    {
+            System.out.println("Your ID:");
+            tmp = scan.nextInt();
+            System.out.print("Your Name: ");
+            Name2 = scan.nextLine();
+            if (Name2.equals(human.get(tmp).Name) == false) {
+                do {
+                    System.out.print("Your Name: ");
+                    Name2 = scan.nextLine();
+                } while (Name2.equals(human.get(tmp).Name) == false);
+                System.out.println("Hello " + human.get(tmp).Name);
+            } else {
+                System.out.println("Hello" + human.get(tmp).Name + " " + human.get(tmp).id);
+            }
+        do {
+            System.out.println("Choose number");
+            System.out.println("1. Feeder your pet");
+            System.out.println("2. TakeForAWalk whit your pet");
+            System.out.println("3. Check account balance");
+            System.out.println("4. Withdraw from account 500");
+            System.out.println("5. Withdraw from account 1500");
+            System.out.println("6. See your dream car");
+            System.out.println("7. Buy your dream car");
+            System.out.println("8. See your car");
+            System.out.println("9. compare two same cars");
+            System.out.println("10. ToString");
+            System.out.print("Your Choose: ");
+            a = scan.nextInt();
+            System.out.println();
+            switch (a) {
+                case 1: {
+                    animal.feed();
+                    System.out.println();
+                }
+                break;
+                case 2: {
+                    animal.takeForAWalk();
+                    if (animal.isLife() == false) {
+                        a = 0;
+                    }
+                    System.out.println();
+                }
+                break;
+                case 3: {
+                    getSalary();
+                    System.out.println();
+                }
+                break;
+                case 4: {
+                    setSalary(500);
+                    System.out.println();
+                }
+                break;
+                case 5: {
+                    setSalary(15000);
+                    System.out.println();
+                }
+                break;
+                case 6: {
+                    getCar();
+                    System.out.println();
+                }
+                break;
+                case 7: {
+                    setCar();
+                    System.out.println();
+                }
+                break;
+                case 8: {
+                    getMyCar();
+                    System.out.println();
+                }
+                break;
+                case 9: {
+                    car.main();
+                    System.out.println();
+                }
+                break;
+                case 10: {
+                    car.main2();
+                    System.out.println();
+                }
+                break;
+                default: {
+                    System.out.println("default");
+                }
+                break;
+            }
+        }while(a !=0);
+    }
 
     public void getMyCar()
     {
@@ -29,7 +149,7 @@ public class Human {
         car.Car("q","1","1",1,1);
     }
 
-    public Boolean setCar(){
+    public boolean setCar(){
         if (car.cost < salary)
         {
             System.out.println("You managed to buy the car for cash");
@@ -59,7 +179,7 @@ public class Human {
     public boolean setSalary(double HowMuch)
     {
         if (salary < HowMuch) {
-            System.out.println("You dont have cash");
+            System.out.println("You don't have cash");
             System.out.println("Current Account status= " + salary);
         }
         else {
